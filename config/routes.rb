7738e42282c6d97915d24b573666d16f738f57df
ctcmc <ccab0528@gmail.com>
2022-09-18 13:52:57 -0700

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  resources :projects
+  devise_for :users, controllers: {registrations: "registrations"}
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root to: "pages#home"
+  devise_scope :user do  
+    get '/users/sign_out' => 'devise/sessions#destroy'     
+ end
+end
